c5d1b27c333630b6ebaffd63e04f14124d95930b
Tomas <tomasdozo@hotmail.com>
2020-09-08 18:32:05 -0300

    V3

--- src/main/java/com/tomasdozo/todolist/ToDoListController.java
+++ src/main/java/com/tomasdozo/todolist/ToDoListController.java
@@ -1,12 +1,8 @@
 package com.tomasdozo.todolist;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -17,8 +13,7 @@ public class ToDoListController {
     @GetMapping ("/get")
     public List<ToDoItem> getMapping (){
         ToDoListService service=ToDoListService.getInstance();
-        List<ToDoItem> l=service.getLista();
-        return l;
+        return service.getLista();
     }
 
 
